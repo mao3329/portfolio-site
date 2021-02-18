d1578a8846589de0e69effedc4dccddda51c0e45
mao3329 <74639010+mao3329@users.noreply.github.com>
2021-02-18 22:10:32 +0900

    Create Portfolio Component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { SiteTop } from './components/Top';
 import { Profile } from './components/Profile';
 import { Skill } from './components/Skill';
+import { Portfolio } from './components/Portfolio';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <SiteTop />
     <Profile />
     <Skill />
+    <Portfolio />
     </>
   );
 }
