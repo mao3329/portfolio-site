bea00a77b64c2d17e0566baaa93472060b042d0e
mao3329 <74639010+mao3329@users.noreply.github.com>
2021-02-18 16:12:20 +0900

    Fix

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 // components
 import { SiteTop } from './components/Top';
 import { Profile } from './components/Profile';
+import { Skill } from './components/Skill';
 
 function App() {
   return (
     <>
     <SiteTop />
     <Profile />
+    <Skill />
     </>
   );
 }
