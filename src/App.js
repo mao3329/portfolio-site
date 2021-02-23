09bea2f32ccab0f0309b8dfc5bc028313a265d07
mao3329 <74639010+mao3329@users.noreply.github.com>
2021-02-23 22:53:15 +0900

    UpdateApp.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import './App.css';
+import styled from 'styled-components';
 
 // components
 import { SiteTop } from './components/Top';
+import { PageMenu } from './components/PageMenu';
 import { Profile } from './components/Profile';
 import { Skill } from './components/Skill';
 import { Portfolio } from './components/Portfolio';
 import { Contact } from './components/Contact';
 
+const ContentTitle = styled.h1`
+
+`;
+
 function App() {
   return (
     <>
-    <SiteTop />
-    <Profile />
-    <Skill />
-    <Portfolio />
-    <Contact />
-    <div className="footer">
-      <div>
-        <p className="copyright">Copyright&copy;  2021  Masaki Takebe.  All Rights Reserved.</p>
+      <SiteTop />
+      <PageMenu />
+      <ContentTitle id="profile" >Profile</ContentTitle>
+      <Profile />
+      <ContentTitle id='skills'>Skills</ContentTitle>
+      <Skill />
+      <ContentTitle id='portfolios' >Portfolios</ContentTitle>
+      <Portfolio />
+      <ContentTitle id='contacts' >Contact</ContentTitle>
+      <Contact />
+      <div className="footer">
+        <div>
+          <p className="copyright">Copyright&copy;  2021  Masaki Takebe.  All Rights Reserved.</p>
+        </div>
       </div>
-    </div>
     </>
   );
 }
